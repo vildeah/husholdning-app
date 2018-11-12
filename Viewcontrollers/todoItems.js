f3be61c9d5919a618ffbc863f1289a416e693e82
Vilde Huseby <vilde.huseby@avoconsulting.no>
2018-11-12 15:40:00 +0100

    Added card-list for done todoItems

--- Viewcontrollers/todoItems.js
+++ Viewcontrollers/todoItems.js
@@ -1,93 +1,108 @@
 ViewControllers.TodoItems = (function() {
+
+    function _fillAccordion (todoItems, accordion, includeButtons) {
+        while (accordion.firstChild) {
+            accordion.removeChild(accordion.firstChild);
+        }
+
+        for (let i = 0; i < todoItems.length; i++) {
+            let id = Core.generateAutoNumber();
+            let cardDiv = document.createElement('div');
+
+            cardDiv.classList.add('card', 'coorgi-card-'+ todoItems[i].difficulty.get(), 'text-white');
+            
+            let cardHeader = document.createElement('div');
+            cardHeader.classList.add('card-header');
+            cardHeader.setAttribute('id', 'heading' + id);
+
+            let h5 = document.createElement('h5');
+            h5.classList.add('mb-0');
+
+            let collapseBtn = document.createElement('button');
+            collapseBtn.classList.add('btn', 'btn-link', 'collapse-header', 'todoItem-header');
+            collapseBtn.setAttribute('data-toggle', 'collapse');
+            collapseBtn.setAttribute('data-target', '#collapse' + id);
+            collapseBtn.setAttribute('aria-expanded', false);
+            collapseBtn.setAttribute('aria-controls', 'collapse'+ id);
+            collapseBtn.innerHTML = todoItems[i].name.get();
+
+            if (includeButtons) {
+                let editBtn = document.createElement('i');
+                editBtn.classList.add('fas', 'fa-pen', 'float-right', 'todoItem-editBtn');
+                editBtn.setAttribute('value', todoItems[i].id.get());
+    
+                let deleteBtn = document.createElement('i');
+                deleteBtn.classList.add('fas', 'fa-trash-alt', 'float-right', 'todoItem-deleteBtn');
+                deleteBtn.setAttribute('value', todoItems[i].id.get());
+    
+                let checkBtn = document.createElement('i');
+                checkBtn.classList.add('fas', 'fa-check', 'float-right', 'todoItem-checkBtn');
+                checkBtn.setAttribute('value', todoItems[i].id.get());
+    
+                h5.appendChild(editBtn);
+                h5.appendChild(deleteBtn);
+                h5.appendChild(checkBtn);
+            }
+
+            h5.appendChild(collapseBtn);
+            cardHeader.appendChild(h5);
+
+            let collapseDiv = document.createElement('div');
+            collapseDiv.classList.add('collapse');
+            collapseDiv.setAttribute('id', 'collapse' + id);
+            collapseDiv.setAttribute('aria-labelledby', 'heading' + id);
+
+            let cardBody = document.createElement('div');
+            cardBody.classList.add('card-body');
+            cardBody.innerHTML = todoItems[i].description.get();
+
+            collapseDiv.appendChild(cardBody);
+            cardDiv.appendChild(cardHeader);
+            cardDiv.appendChild(collapseDiv);
+            accordion.appendChild(cardDiv);
+        }
+
+        let editBtns = document.getElementsByClassName('todoItem-editBtn');
+        for (let i = 0; i < editBtns.length; i++) {
+            editBtns[i].onclick = function () {
+                let todoItemId = parseInt(editBtns[i].getAttribute('value'));
+                ViewControllers.TodoItems.renderEditTodoItemModal(ModelControllers.TodoItems.getById(todoItemId));
+            }
+        }
+
+        let deleteBtns = document.getElementsByClassName('todoItem-deleteBtn');
+        for (let i = 0; i < deleteBtns.length; i++) {
+            deleteBtns[i].onclick = function () {
+                let modal = document.getElementById('deleteItem__modal');
+                $(modal).modal('show');
+                
+                document.getElementById('confirm__deletion__button').onclick = function() {
+                    ViewControllers.TodoItems.deleteTodoItem(parseInt(deleteBtns[i].getAttribute('value')));
+                    $(modal).modal('hide');
+                }
+            }
+        }
+
+        let checkBtns = document.getElementsByClassName('todoItem-checkBtn');
+        for (let i = 0; i < checkBtns.length; i++) {
+            checkBtns[i].onclick = function () {
+                let todoItemId = parseInt(checkBtns[i].getAttribute('value'));
+                ViewControllers.TodoItems.completeTodoItem(todoItemId);
+            }
+        }
+    }
+
     return {
         renderCards: function() {
             let todoItems = ModelControllers.TodoItems.getNotDoneItems();
             let accordion = document.getElementById('accordion');
-            while (accordion.firstChild) {
-                accordion.removeChild(accordion.firstChild);
-            }
-            for (let i = 0; i < todoItems.length; i++) {
-                let cardDiv = document.createElement('div');
+            _fillAccordion(todoItems, accordion, true);
 
-                cardDiv.classList.add('card', 'coorgi-card-'+ todoItems[i].difficulty.get(), 'text-white');
-                
-                let cardHeader = document.createElement('div');
-                cardHeader.classList.add('card-header');
-                cardHeader.setAttribute('id', 'heading' + i);
+            let signedInMember = ModelControllers.Members.getSignedInMember();
+            let doneTodoItems = signedInMember.doneTodoItems.get();
+            let doneAccordion = document.getElementById('doneTodoItems--accordion');
+            _fillAccordion(doneTodoItems, doneAccordion, false);
 
-                let h5 = document.createElement('h5');
-                h5.classList.add('mb-0');
-
-                let collapseBtn = document.createElement('button');
-                collapseBtn.classList.add('btn', 'btn-link', 'collapse-header', 'todoItem-header');
-                collapseBtn.setAttribute('data-toggle', 'collapse');
-                collapseBtn.setAttribute('data-target', '#collapse' + i);
-                collapseBtn.setAttribute('aria-expanded', false);
-                collapseBtn.setAttribute('aria-controls', 'collapse'+ i);
-                collapseBtn.innerHTML = todoItems[i].name.get();
-
-                let editBtn = document.createElement('i');
-                editBtn.classList.add('fas', 'fa-pen', 'float-right', 'todoItem-editBtn');
-                editBtn.setAttribute('value', todoItems[i].id.get());
-
-                let deleteBtn = document.createElement('i');
-                deleteBtn.classList.add('fas', 'fa-trash-alt', 'float-right', 'todoItem-deleteBtn');
-                deleteBtn.setAttribute('value', todoItems[i].id.get());
-
-                let checkBtn = document.createElement('i');
-                checkBtn.classList.add('fas', 'fa-check', 'float-right', 'todoItem-checkBtn');
-                checkBtn.setAttribute('value', todoItems[i].id.get());
-
-                h5.appendChild(collapseBtn);
-                h5.appendChild(editBtn);
-                h5.appendChild(deleteBtn);
-                h5.appendChild(checkBtn);
-                cardHeader.appendChild(h5);
-
-                let collapseDiv = document.createElement('div');
-                collapseDiv.classList.add('collapse');
-                collapseDiv.setAttribute('id', 'collapse' + i);
-                collapseDiv.setAttribute('aria-labelledby', 'heading' + i);
-                collapseDiv.setAttribute('data-parent', '#accordion');
-
-                let cardBody = document.createElement('div');
-                cardBody.classList.add('card-body');
-                cardBody.innerHTML = todoItems[i].description.get();
-
-                collapseDiv.appendChild(cardBody);
-                cardDiv.appendChild(cardHeader);
-                cardDiv.appendChild(collapseDiv);
-                accordion.appendChild(cardDiv);
-            }
-
-            let editBtns = document.getElementsByClassName('todoItem-editBtn');
-            for (let i = 0; i < editBtns.length; i++) {
-                editBtns[i].onclick = function () {
-                    let todoItemId = parseInt(editBtns[i].getAttribute('value'));
-                    ViewControllers.TodoItems.renderEditTodoItemModal(ModelControllers.TodoItems.getById(todoItemId));
-                }
-            }
-
-            let deleteBtns = document.getElementsByClassName('todoItem-deleteBtn');
-            for (let i = 0; i < deleteBtns.length; i++) {
-                deleteBtns[i].onclick = function () {
-                    let modal = document.getElementById('deleteItem__modal');
-                    $(modal).modal('show');
-                    
-                    document.getElementById('confirm__deletion__button').onclick = function() {
-                        ViewControllers.TodoItems.deleteTodoItem(parseInt(deleteBtns[i].getAttribute('value')));
-                        $(modal).modal('hide');
-                    }
-                }
-            }
-
-            let checkBtns = document.getElementsByClassName('todoItem-checkBtn');
-            for (let i = 0; i < checkBtns.length; i++) {
-                checkBtns[i].onclick = function () {
-                    let todoItemId = parseInt(checkBtns[i].getAttribute('value'));
-                    ViewControllers.TodoItems.completeTodoItem(todoItemId);
-                }
-            }
         },
 
         completeTodoItem: function (todoItemId) {
